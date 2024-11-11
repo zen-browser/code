6e6e58c2d57048ed37c159b67a0844a5516e9a02
Kristijan Ribarić <kriba13@gmail.com>
2024-11-11 15:19:42 +0100

    Prevent pins deletion from database on non explicit tab closes (window closing)
    Previously, if you had multiple windows opened and you closed one, it could trigger TabClose event and delete the pins from zen_pins table. With this change the pins are deleted only on explicit tab closing from tab context menu.

--- src/browser/base/zen-components/ZenPinnedTabManager.mjs
+++ src/browser/base/zen-components/ZenPinnedTabManager.mjs
@@ -2,7 +2,7 @@
   const lazy = {};
 
   class ZenPinnedTabsObserver {
-    static ALL_EVENTS = ['TabPinned', 'TabUnpinned', 'TabClose'];
+    static ALL_EVENTS = ['TabPinned', 'TabUnpinned'];
 
     #listeners = [];
 
@@ -193,9 +193,6 @@
             delete tab._zenClickEventListener;
           }
           break;
-        case "TabClose":
-          this._removePinnedAttributes(tab);
-          break;
         default:
           console.warn('ZenPinnedTabManager: Unhandled tab event', action);
           break;
@@ -284,18 +281,20 @@
       await this._refreshPinnedTabs();
     }
 
-    async _removePinnedAttributes(tab) {
+    async _removePinnedAttributes(tab, isClosing = false) {
       if(!tab.getAttribute("zen-pin-id")) {
         return;
       }
 
       await ZenPinnedTabsStorage.removePin(tab.getAttribute("zen-pin-id"));
 
-      tab.removeAttribute("zen-pin-id");
+      if(!isClosing) {
+        tab.removeAttribute("zen-pin-id");
 
-      if(!tab.hasAttribute("zen-workspace-id") && ZenWorkspaces.workspaceEnabled) {
-        const workspace = await ZenWorkspaces.getActiveWorkspace();
-        tab.setAttribute("zen-workspace-id", workspace.uuid);
+        if (!tab.hasAttribute("zen-workspace-id") && ZenWorkspaces.workspaceEnabled) {
+          const workspace = await ZenWorkspaces.getActiveWorkspace();
+          tab.setAttribute("zen-workspace-id", workspace.uuid);
+        }
       }
 
       await this._refreshPinnedTabs();
@@ -323,6 +322,7 @@
 
       switch (behavior) {
         case 'close':
+          this._removePinnedAttributes(selectedTab, true);
           gBrowser.removeTab(selectedTab, { animate: true });
           break;
         case 'reset-unload-switch':
@@ -458,6 +458,7 @@
       document.getElementById('context_pinTab')?.after(element);
     }
 
+    // TODO: remove this as it's not possible to know the base pinned url any more as it's now stored in tab state
     resetPinnedTabData(tabData) {
       if (lazy.zenPinnedTabRestorePinnedTabsToPinnedUrl && tabData.pinned && tabData.zenPinnedEntry) {
         tabData.entries = [JSON.parse(tabData.zenPinnedEntry)];

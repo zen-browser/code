947488f144e4b94db8e90abe1946d964240aa462
Cristian Cezar Moisés <ethicalhacker@riseup.net>
2025-01-03 02:19:47 +0000

    Update download-language-packs.sh
    The script can improve:
        - Variable Usage: Introduced variables to store paths and constants to avoid repeating strings and reduce the chance of errors.
        - Directory Creation: Used mkdir -p to create the localization directory if it doesn't exist.
        - Readability: Used while IFS= read -r to read the language file, which is safer and handles spaces correctly.
        - Removed Unnecessary cd Commands: Reduced the number of directory changes by using absolute paths.
        - Consistent Quoting: Added quotes around variables to handle paths with spaces properly.
        - Script Shebang: Added #!/bin/bash for clarity on the script's intended interpreter.
    
    
    Signed-off-by: Cristian Cezar Moisés <ethicalhacker@riseup.net>

--- scripts/download-language-packs.sh
+++ scripts/download-language-packs.sh
@@ -1,51 +1,53 @@
-
+#!/bin/bash
 set -ex
 
+# Define constants
 CURRENT_DIR=$(pwd)
+L10N_DIR="$CURRENT_DIR/l10n"
+FIREFOX_L10N_REPO="https://github.com/mozilla-l10n/firefox-l10n"
+LANGUAGES_FILE="$L10N_DIR/supported-languages"
+TOOLS_DIR=~/tools
+GIT_CINNABAR_DIR="$TOOLS_DIR/git-cinnabar"
 
+# Configure git
 git config --global init.defaultBranch main
 git config --global fetch.prune true
 
-cd $CURRENT_DIR
-
-cd ./l10n
-git clone https://github.com/mozilla-l10n/firefox-l10n
-cd $CURRENT_DIR
+# Clone the repository
+mkdir -p "$L10N_DIR"
+git clone "$FIREFOX_L10N_REPO" "$L10N_DIR/firefox-l10n"
 
 update_language() {
-  langId=$1
-  cd ./l10n
-  cd $langId
-
+  local langId=$1
   echo "Updating $langId"
-  # move the contents from ../firefox-l10n/$langId to ./l10n/$langId
-  rsync -av --progress ../firefox-l10n/$langId/ . --exclude .git
-
-  cd $CURRENT_DIR
+  
+  # Use rsync to move the contents, excluding the .git directory
+  rsync -av --progress "$L10N_DIR/firefox-l10n/$langId/" "$L10N_DIR/$langId/" --exclude .git
 }
 
-export PATH=~/tools/git-cinnabar:$PATH
-for lang in $(cat ./l10n/supported-languages); do
-  update_language $lang
-done
-cd $CURRENT_DIR
+# Ensure git-cinnabar is in the PATH
+export PATH="$GIT_CINNABAR_DIR:$PATH"
+
+# Update each language
+while IFS= read -r lang; do
+  update_language "$lang"
+done < "$LANGUAGES_FILE"
 
 # Move all the files to the correct location
-
-sh scripts/copy-language-pack.sh en-US
-for lang in $(cat ./l10n/supported-languages); do
-  sh scripts/copy-language-pack.sh $lang
+for lang in $(cat "$LANGUAGES_FILE"); do
+  sh scripts/copy-language-pack.sh "$lang"
 done
 
 wait
 
+# Clean up
 echo "Cleaning up"
-rm -rf ~/tools
+rm -rf "$TOOLS_DIR"
 rm -rf ~/.git-cinnabar
 
-for lang in $(cat ./l10n/supported-languages); do
-  # remove every file except if it starts with "zen"
-  find ./l10n/$lang -type f -not -name "zen*" -delete
-done
+# Remove files that do not start with "zen"
+while IFS= read -r lang; do
+  find "$L10N_DIR/$lang" -type f -not -name "zen*" -delete
+done < "$LANGUAGES_FILE"
 
-rm -rf ./l10n/firefox-l10n
+rm -rf "$L10N_DIR/firefox-l10n"

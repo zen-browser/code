75cbb0eeb2fe7bdc2f92317cf4e641c2f5b5090e
mr. M <mauro.balades@tutanota.com>
2025-01-05 20:22:30 +0100

    Add macOS specific preference for greyed-out inactive windows

--- src/browser/app/profile/zen-browser.js
+++ src/browser/app/profile/zen-browser.js
@@ -145,7 +145,12 @@ pref('zen.view.use-single-toolbar', true);
 pref('zen.view.sidebar-expanded', true);
 pref('zen.view.sidebar-collapsed.hide-mute-button', true);
 pref('zen.view.experimental-force-window-controls-left', false);
+
+#ifdef XP_MACOSX
+pref('zen.view.grey-out-inactive-windows', false);
+#else
 pref('zen.view.grey-out-inactive-windows', true);
+#endif
 
 pref('zen.view.hide-window-controls', true);
 pref('zen.view.experimental-no-window-controls', false);

b8aa560d527718eb1c11157cd14f543be47380c1
mr. M <mauro.balades@tutanota.com>
2025-02-01 22:53:21 +0100

    Glance can now be on multiple tabs and improved animations

--- src/browser/base/content/ZenUIManager.mjs
+++ src/browser/base/content/ZenUIManager.mjs
@@ -86,9 +86,7 @@ var gZenUIManager = {
   },
 
   generateUuidv4() {
-    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
-      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16)
-    );
+    return Services.uuid.generateUUID().toString();
   },
 
   toogleBookmarksSidebar() {
